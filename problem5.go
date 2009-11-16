8436bc2f06790902ae7e78cea4fbf92b39d966ef
Josh Goebel <dreamer3@gmail.com>
2009-11-15 20:08:03 -0500

    add solution to problem 5

---
+++ problem5.go
@@ -0,0 +1,34 @@
+// Problem 5 - http://projecteuler.net/
+//
+// What is the smallest number that is evenly divisible by 
+// all of the numbers from 1 to 20?
+
+package main
+
+import (
+  "fmt";
+)
+
+func divisible(n int64) bool
+{
+  // we can do < 20 because we increment in units of 20 in the
+  // main loop so we don't need to check divisibility by 20
+  for i:=int64(1); i<20; i++ {
+    if n%i != 0 {
+      return false;
+    }
+  }
+  return true
+}
+
+func main()
+{
+  best := int64(20);
+  for {
+    if divisible(best) {
+      break
+    } 
+    best += 20
+  }
+  fmt.Printf("%d\n", best)
+}
