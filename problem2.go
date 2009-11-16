0dd21fba582759eb10a5350256428c9bf9bb59b4
Josh Goebel <jgoebel@Josh-Goebels-MacBook-Pro.local>
2009-11-15 19:57:50 -0500

    first 4 problems

---
+++ problem2.go
@@ -0,0 +1,20 @@
+// Problem 2 - http://projecteuler.net/
+//
+// Find the sum of all the even-valued terms in Fib
+// the sequence which do not exceed four million.
+
+package main
+
+import "fmt";
+
+func main()
+{
+  sum := 0;
+  for a, b := 1, 2; b<4000000;{
+    if b%2 == 0 {
+      sum += b
+    }
+    a,b = b, b+a
+  }
+  fmt.Printf("%d\n", sum)
+}
---
+++ problem3.go
@@ -0,0 +1,34 @@
+// Problem 3 - http://projecteuler.net/
+//
+// Find the largest prime factor of a composite number
+
+package main
+
+import "fmt";
+
+func prime(number int64) bool
+{
+  for i := int64(2); i< number; i++ {
+    if number%i==0 {
+      return false;
+    }
+  }
+  return true;
+}
+
+func main()
+{
+  largest := int64(0);
+  opposite := int64(0);
+  composite := int64(600851475143);
+  for i := int64(2); i<composite; i++ {
+    if composite%i==0 {
+      opposite = composite/i;
+      if prime(opposite) {
+        largest = opposite;
+        break;
+      }
+    }
+  }
+  fmt.Printf("%d\n", largest)
+}
